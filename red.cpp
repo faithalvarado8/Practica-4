ce9057b15d35984d92d874caebfe939be3007b2a
Faith Alvarado <faith.alvarado@udea.edu.co>
2024-10-21 12:00:55 -0500

    Update red.cpp

--- red.cpp
+++ red.cpp
@@ -1,21 +1,54 @@
 #include "red.h"
-#include <iostream>
-#include <vector>
-#include <algorithm>
 
-void Red::agregarEnrutador(const Enrutador& enrutador) {
-    enrutadores.push_back(enrutador);
+void Red::agregarEnrutador(const string& nombre) {
+    if (enrutadores.find(nombre) == enrutadores.end()) {
+        enrutadores[nombre] = Enrutador(nombre);
+    }
 }
 
-void Red::eliminarEnrutador(const string& nombreEnrutador) {
-    enrutadores.erase(remove_if(enrutadores.begin(), enrutadores.end(),
-    [&](const Enrutador& enrutador) { return enrutador.getNombre() == nombreEnrutador; }),
-    enrutadores.end());
+void Red::eliminarEnrutador(const string& nombre) {
+    //Elimina el enrutador del mapa de enrutadores
+    enrutadores.erase(nombre);
+
+    //Elimina las rutas hacia el enrutador eliminado en las tablas de otros enrutadores
+    for (auto& [nombreEnrutador, enrutador] : enrutadores) {
+        map<string, int>& tabla = enrutador.getTabla();
+        auto it = tabla.find(nombre);
+        if (it != tabla.end()) {
+            tabla.erase(it);
+        }
+    }
+}
+
+void Red::agregarRuta(const string& origen, const string& destino, int costo) {
+    agregarEnrutador(origen);
+    agregarEnrutador(destino);
+    enrutadores[origen].costoRuta(destino, costo);
+}
+
+void Red::cargarDesdeArchivo(const string& nombreArchivo) {
+    ifstream archivo(nombreArchivo);
+    if (!archivo.is_open()) {
+        cerr << "No se pudo abrir el archivo " << nombreArchivo << "\n";
+        return;
+    }
+
+    string linea;
+    while (getline(archivo, linea)) {
+        istringstream streamLinea(linea);
+        string origen, destino;
+        int costo;
+
+        if (streamLinea >> origen >> destino >> costo) {
+            agregarRuta(origen, destino, costo);
+        }
+    }
+
+    archivo.close();
 }
 
 void Red::mostrarRed() const {
-    cout << "Estado de la red: " << endl;
-    for (const auto& enrutador : enrutadores) {
+    for (const auto& [nombre, enrutador] : enrutadores) {
         enrutador.mostrarTabla();
     }
 }
