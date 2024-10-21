01141e2daab9f9f2cd8459b8f65bb53624ba1604
Faith Alvarado <faith.alvarado@udea.edu.co>
2024-10-21 12:02:47 -0500

    Update enrutador.cpp

--- enrutador.cpp
+++ enrutador.cpp
@@ -1,30 +1,25 @@
-#include "enrutador.h"
+#include "Enrutador.h"
 
-#include <iostream>
-#include <map>
-#include <string>
+Enrutador::Enrutador(){
+}
 
 Enrutador::Enrutador(const string& nombreEnrutador) : nombre(nombreEnrutador) {}
 
-void Enrutador::agregarRuta(const string& destino, int costo) {
+void Enrutador::costoRuta(const string& destino, int costo) {
     tablaEnrutamiento[destino] = costo;
 }
 
-void Enrutador::actualizarRuta(const string& destino, int nuevoCosto) {
-    if (tablaEnrutamiento.find(destino) != tablaEnrutamiento.end()) {
-        tablaEnrutamiento[destino] = nuevoCosto;
-    }
-}
-
-void Enrutador::mostrarTabla() const {
-    cout << "Tabla de enrutamiento para " << nombre << ":" << endl;
-    for (const auto& entrada : tablaEnrutamiento) {
-        cout << "Destino: " << entrada.first << " - Costo: " << entrada.second << endl;
-    }
-    cout << endl << endl;
+map<string, int> Enrutador::getTabla() const {
+    return tablaEnrutamiento;
 }
 
 string Enrutador::getNombre() const {
     return nombre;
 }
 
+void Enrutador::mostrarTabla() const {
+    cout << "Tabla de enrutamiento para " << nombre << ":\n";
+    for (const auto& entrada : tablaEnrutamiento) {
+        cout << "Destino: " << entrada.first << " - Costo: " << entrada.second << "\n";
+    }
+}
