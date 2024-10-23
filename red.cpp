f6af357690d50674a662c528e0f31d7293699428
Faith Alvarado <faith.alvarado@udea.edu.co>
2024-10-22 19:41:25 -0500

    Update red.cpp
    Implementacion de metodos para mostrar ruta mas corta y su costo

--- red.cpp
+++ red.cpp
@@ -7,12 +7,10 @@ void Red::agregarEnrutador(const string& nombre) {
 }
 
 void Red::eliminarEnrutador(const string& nombre) {
-    //Elimina el enrutador del mapa de enrutadores
     enrutadores.erase(nombre);
 
-    //Elimina las rutas hacia el enrutador eliminado en las tablas de otros enrutadores
     for (auto& [nombreEnrutador, enrutador] : enrutadores) {
-        map<string, int>& tabla = enrutador.getTabla();
+        map<string, float>& tabla = enrutador.getTabla();
         auto it = tabla.find(nombre);
         if (it != tabla.end()) {
             tabla.erase(it);
@@ -20,11 +18,20 @@ void Red::eliminarEnrutador(const string& nombre) {
     }
 }
 
-void Red::agregarRuta(const string& origen, const string& destino, int costo) {
-    agregarEnrutador(origen);
-    agregarEnrutador(destino);
+void Red::agregarRuta(const string& origen, const string& destino, float costo) {
+    if (enrutadores.find(origen) == enrutadores.end()) {
+        enrutadores[origen] = Enrutador(origen);
+    }
+    if (enrutadores.find(destino) == enrutadores.end()) {
+        enrutadores[destino] = Enrutador(destino);
+    }
+
     enrutadores[origen].costoRuta(destino, costo);
+    enrutadores[destino].costoRuta(origen, costo);
+
+    cout << "Ruta agregada: " << origen << " -> " << destino << " (Costo: " << costo << ")" << endl;
 }
+
 
 void Red::cargarDesdeArchivo(const string& nombreArchivo) {
     ifstream archivo(nombreArchivo);
@@ -37,7 +44,7 @@ void Red::cargarDesdeArchivo(const string& nombreArchivo) {
     while (getline(archivo, linea)) {
         istringstream streamLinea(linea);
         string origen, destino;
-        int costo;
+        float costo;
 
         if (streamLinea >> origen >> destino >> costo) {
             agregarRuta(origen, destino, costo);
@@ -47,8 +54,75 @@ void Red::cargarDesdeArchivo(const string& nombreArchivo) {
     archivo.close();
 }
 
+void Red::caminoMasCorto(string origen, string destino) {
+    if (enrutadores.find(origen) == enrutadores.end() || enrutadores.find(destino) == enrutadores.end()) {
+        cout << "Uno de los enrutadores no existe en la red." << endl;
+        return;
+    }
+
+    map<string, float> distancias;
+    map<string, string> anterior;
+    set<pair<float, string>> cola;
+
+    for (const auto &enrutador : enrutadores) {
+        distancias[enrutador.first] = numeric_limits<float>::infinity();  // Inicialmente, todas las distancias son infinitas
+    }
+    distancias[origen] = 0;
+    cola.insert({0, origen});
+
+    while (!cola.empty()) {
+        auto actual = *cola.begin();
+        cola.erase(cola.begin());
+        string enrutadorActual = actual.second;
+
+        if (enrutadorActual == destino) {
+            break;
+        }
+
+        for (const auto &vecino : enrutadores[enrutadorActual].getTabla()) {
+            string enrutadorVecino = vecino.first;
+            float costoRuta = vecino.second;
+
+            if (distancias[enrutadorActual] + costoRuta < distancias[enrutadorVecino]) {
+                cola.erase({distancias[enrutadorVecino], enrutadorVecino});
+                distancias[enrutadorVecino] = distancias[enrutadorActual] + costoRuta;
+                anterior[enrutadorVecino] = enrutadorActual;
+                cola.insert({distancias[enrutadorVecino], enrutadorVecino});
+            }
+        }
+    }
+
+    if (distancias[destino] == numeric_limits<float>::infinity()) {
+        cout << "No hay un camino disponible desde " << origen << " hasta " << destino << "." << endl;
+    } else {
+        cout << "El costo minimo desde " << origen << " hasta " << destino << " es: " << distancias[destino] << endl;
+        cout << "El camino es: ";
+
+        vector<string> camino;
+        for (string enrutador = destino; enrutador != origen; enrutador = anterior[enrutador]) {
+            camino.push_back(enrutador);
+        }
+        camino.push_back(origen);
+
+        for (auto it = camino.rbegin(); it != camino.rend(); ++it) {
+            cout << *it;
+            if (it + 1 != camino.rend()) {
+                cout << " -> ";
+            }
+        }
+        cout << endl;
+    }
+}
+
+
 void Red::mostrarRed() const {
     for (const auto& [nombre, enrutador] : enrutadores) {
         enrutador.mostrarTabla();
     }
 }
+
+void Red::borrarRed() {
+    enrutadores.clear();
+    cout << "Todos los datos de la red han sido borrados." << endl;
+}
+
